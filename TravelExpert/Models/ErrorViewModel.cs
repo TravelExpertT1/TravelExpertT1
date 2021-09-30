c65e9ad0edfac9acec177298ebf40c73eca42f27
suzydevworks <83732243+suzydevworks@users.noreply.github.com>
2021-09-30 17:20:30 -0600

    Add project files.

---
+++ TravelExpert/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TravelExpert.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
