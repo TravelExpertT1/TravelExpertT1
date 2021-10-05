78b2595f33d76c4560026d605b713b058b7a00c8
mohannad786 <83979479+mohannad786@users.noreply.github.com>
2021-10-05 11:42:03 -0600

    Add project files.

---
+++ TravelExpert/Models/DomainModels/Affiliation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+#nullable disable
+
+namespace TravelExpert.Models
+{
+    public partial class Affiliation
+    {
+        public Affiliation()
+        {
+            SupplierContacts = new HashSet<SupplierContact>();
+        }
+
+        [Key]
+        [StringLength(10)]
+        public string AffilitationId { get; set; }
+        [StringLength(50)]
+        public string AffName { get; set; }
+        [StringLength(50)]
+        public string AffDesc { get; set; }
+
+        [InverseProperty(nameof(SupplierContact.Affiliation))]
+        public virtual ICollection<SupplierContact> SupplierContacts { get; set; }
+    }
+}
---
+++ TravelExpert/Startup.cs
@@ -0,0 +1,99 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TravelExpert.Models;
+
+namespace TravelExpert
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRouting(options => options.LowercaseUrls = true);
+
+            services.AddMemoryCache();
+            services.AddSession();
+
+            services.AddControllersWithViews().AddNewtonsoftJson();
+
+            services.AddDbContext<TravelExpertsContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("TravelExpertsContext")));
+
+            // add this
+            services.AddIdentity<User, IdentityRole>(options => {
+                options.Password.RequiredLength = 6;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireDigit = false;
+            }).AddEntityFrameworkStores<TravelExpertsContext>()
+              .AddDefaultTokenProviders();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();   // add this
+            app.UseAuthorization();    // add this
+
+            app.UseSession();
+
+            app.UseEndpoints(endpoints =>
+            {
+                // route for Admin area
+                endpoints.MapAreaControllerRoute(
+                    name: "admin",
+                    areaName: "Admin",
+                    pattern: "Admin/{controller=Book}/{action=Index}/{id?}");
+
+                // route for paging, sorting, and filtering
+                endpoints.MapControllerRoute(
+                    name: "",
+                    pattern: "{controller}/{action}/page/{pagenumber}/size/{pagesize}/sort/{sortfield}/{sortdirection}/filter/{author}/{genre}/{price}");
+
+                // route for paging and sorting only
+                endpoints.MapControllerRoute(
+                    name: "",
+                    pattern: "{controller}/{action}/page/{pagenumber}/size/{pagesize}/sort/{sortfield}/{sortdirection}");
+
+                // default route
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}/{slug?}");
+            });
+
+            TravelExpertsContext.CreateAdminUser(app.ApplicationServices).Wait();
+        }
+        }
+    }
+
